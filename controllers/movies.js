80878fbf4828b166ca87e87ff81b1bcd96d2a5eb
mickovi <m.oviedo@uni.pe>
2023-08-14 15:26:25 -0500

    move the api architecture to the MVC

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,4 +1,4 @@
-import { MovieModel } from '../models/database/movie.js'
+import { MovieModel } from '../models/local/movie.js'
 import { validateMovie, validatePartialMovie } from '../schemas/movies.js'
 
 export class MovieController {
